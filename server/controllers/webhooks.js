9419b607c7b7f0bbaca2ec9dcdad74a101513ec9
Ritik Sharma <ritiksharma32008@gmail.com>
2025-07-15 10:16:47 +0530

    webhook.js updated

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -52,3 +52,25 @@ export const clerkWebhooks = async(req,res)=>{
         res.json({success:false , message:error.message})
     }
 }
+
+/*
+Webhook is a way for one server(like clerk) to send real-time data to another server(like your backend) , when an event occurs like user being created , updated , deleted.
+svix is library used to verify the authenticity of webhook(clerk uses svix under the hood to send webhooks)
+User is Mongoose model to store user data in MongoDB database.
+CLERK_WEBHOOK_SECRET is secret key (from clerk Dashboard) that ensure only clerk an send valid data to my server.
+const headers = {
+  "svix-id": req.headers["svix-id"],
+  "svix-timestamp": req.headers["svix-timestamp"],
+  "svix-signature": req.headers["svix-signature"],
+};
+These headers are automatically added by Clerk. They’re needed for Svix to validate the webhook.
+
+Actually what happen :
+clerk uses Svix under the hood to send webhooks.
+When clerk sends you a Webhook , svix digitally sign the payload using a secret key(Your CLERK_WEBHOOK_SECRET)
+Your server uses these headers and secret key to verify that the request really came from clerk not from hacker or fake bot.
+This process is called signature verification.
+
+clerk is user authentication and management service designed for modern web .
+It helps you to add login , signup and user profile features to your app without building everything from scratch. 
+ */
